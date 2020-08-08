3ad73d5ebeaf749fb98cc9088706ae89102b87ef
YC <YangC>
2020-08-08 15:17:19 +0800

    这是一个C++代码

---
+++ 这是一个C++代码/这是一个C++代码/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  这是一个C++代码
+//
+//  Created by 超哥 on 2020/8/8.
+//  Copyright © 2020 超哥. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
